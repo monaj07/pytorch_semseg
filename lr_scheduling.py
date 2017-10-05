2c4c35bd937351a5d05bacc6bf0b20601c54a16b
Mohammad Najafi <najafi.ece@gmail.com>
2017-10-05 20:14:26 +0100

    Added self_supervised_learning module.
    It trains Alexnet by performing the task of classifying video frames to their parent video clip. The video dataset contains N video clips (N folders of successive video frames) and each clips is represented by a class label.
    It is yet to be evaluated by using its learned weights for the fine0tuning stage on Pascal image segmentation.
    
    There are some points that should be taken into account. For instance, the image scale should be quite larger than image crop size to account for a larger variety in the selected random crops from the video frames and lowering the danger of overfitting the video frame classifier.
    
    lr_scheduling is updated to receive step as its argument rather than having it fixed within the function.

--- lr_scheduling.py
+++ lr_scheduling.py
@@ -22,7 +22,7 @@ def adjust_learning_rate(optimizer, init_lr, epoch):
 
 import pdb
 
-def adjust_learning_rate_v2(optimizer, optimizer_init, epoch):
+def adjust_learning_rate_v2(optimizer, optimizer_init, epoch, step=20):
     """Sets the learning rate to the initial LR decayed by 2 every 20 epochs"""
     for (param_group, param_group_init) in zip(optimizer.param_groups, optimizer_init.param_groups):
-        param_group['lr'] = param_group_init['lr'] * (0.5 ** (epoch // 20))
+        param_group['lr'] = param_group_init['lr'] * (0.5 ** (epoch // step))
--- self_supervised_learning.py
+++ self_supervised_learning.py
@@ -1,3 +1,9 @@
+"""
+This is the code for Self-Supervised Learning.
+The goal is to learn image feature maps using video data.
+The idea is to assign the frames within each video a unique class label, and perform a classification.
+"""
+
 import sys
 import torch
 import visdom
@@ -8,6 +14,10 @@ import torch.nn.functional as F
 import torchvision.models as models
 import matplotlib.pyplot as plt
 import torch.nn.init as init
+import torchvision.datasets as dset
+from torchvision import transforms
+
+from networks import AlexNet
 
 from torch.autograd import Variable
 from torch.utils import data
@@ -21,23 +31,13 @@ import pdb
 import copy
 
 ############################################
-# Padding layer to be applied to the input data
-class padder_layer(nn.Module):
-    def __init__(self, pad_size):
-        super(padder_layer, self).__init__()
-        self.apply_padding = nn.ConstantPad2d(pad_size, 0)  # Pad with zero values
-
-    def forward(self, x):
-        output = self.apply_padding(x)
-        return output
-############################################
-
-############################################
 # Parameter initialization for the network layers
 # It should be applied to the network using `apply` function
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
+        init.xavier_normal(m.weight)
+    elif classname.find('Linear') != -1:
         init.xavier_normal(m.weight)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
@@ -48,61 +48,68 @@ def weights_init(m):
 def train(args):
     ############################################
     # Setup Dataloader
-    data_loader = get_loader(args.dataset)
     data_path = get_data_path(args.dataset)
-    loader = data_loader(data_path, split=args.split, is_transform=True, img_size=(args.img_rows, args.img_cols))
-    n_classes = loader.n_classes
-    trainloader = data.DataLoader(loader, batch_size=args.batch_size, num_workers=4, shuffle=True)
+    dataset = dset.ImageFolder(root=data_path,
+                               transform=transforms.Compose([
+                                   transforms.Scale(args.img_rows),
+                                   transforms.RandomCrop(args.img_rows),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
+
+    n_classes = len(dataset.classes)
+    dataloader = data.DataLoader(dataset, batch_size=args.batch_size, num_workers=4, shuffle=True)
     ############################################
 
+    ######## pretrained = FALSE ===>>> NO PRE-TRAINING ########
     ############################################
     # Setup Model
-    pre_trained = True
-    netF, netS = get_model(args.arch, n_classes, pre_trained=pre_trained)
-    padder = padder_layer(pad_size=100)
+    """
+    model = models.alexnet(pretrained=False)
+    block = list(model.classifier.children())
+    old_layer = block.pop()
+    num_feats = old_layer.in_features
+    block.append(nn.Linear(num_feats, n_classes))
+    model.classifier = nn.Sequential(*block)
+    """
+    model = AlexNet(num_classes=n_classes)
     ############################################
 
     ############################################
     # Random weight initialization
-    if not pre_trained:
-        netF.apply(weights_init)
-        netS.apply(weights_init)
+    model.apply(weights_init)
     ############################################
 
     ############################################
     # If resuming the training from a saved model
-    if args.netF_path != '':
+    if args.model_path != '':
         print('\n' + '-' * 40)
-        netF = torch.load(args.netF_path)
-        print('Restored the trained network {}'.format(args.netF_path))
-    if args.netS_path != '':
-        netS = torch.load(args.netS_path)
-        print('Restored the trained network {}'.format(args.netS_path))
-        print('-' * 40)
+        model = torch.load(args.model_path)
+        print('Restored the trained network {}'.format(args.model_path))
     ############################################
 
 
     ############################################
+    criterion = nn.CrossEntropyLoss() # Loss criterion
     # Porting the networks to CUDA
     if torch.cuda.is_available():
-        netF.cuda(args.gpu)
-        netS.cuda(args.gpu)
-        padder.cuda(args.gpu)
+        model.cuda(args.gpu)
+        criterion.cuda(args.gpu)
     ############################################
 
     ############################################
     # Defining the optimizer over the network parameters
-    optimizerSS = torch.optim.SGD([{'params': netF.features.parameters()},
-                                        {'params': netF.classifier.parameters(), 'lr':10*args.l_rate},
-                                        {'params': netS.parameters(), 'lr':20*args.l_rate}],
-                                  lr=args.l_rate, momentum=0.99, weight_decay=5e-4)
+    optimizerSS = torch.optim.SGD([{'params': model.features.parameters()},
+                                        {'params': model.classifier.parameters(), 'lr':1*args.l_rate}],
+                                  lr=args.l_rate, momentum=0.9, weight_decay=5e-4)
     optimizerSS_init = copy.deepcopy(optimizerSS)
     ############################################
 
     ############################################
     # TRAINING:
     for epoch in range(args.n_epoch):
-        for i, (images, labels) in enumerate(trainloader):
+        for i, (images, labels) in enumerate(dataloader):
             ######################
             # Porting the data to Autograd variables and CUDA (if available)
             if torch.cuda.is_available():
@@ -115,24 +122,20 @@ def train(args):
             ######################
             # Scheduling the learning rate
             #adjust_learning_rate(optimizerSS, args.l_rate, epoch)
-            adjust_learning_rate_v2(optimizerSS, optimizerSS_init, epoch, step=20)
+            adjust_learning_rate_v2(optimizerSS, optimizerSS_init, epoch, step=10)
 
             ######################
             # Setting the gradients to zero at each iteration
             optimizerSS.zero_grad()
-            netF.zero_grad()
-            netS.zero_grad()
+            model.zero_grad()
 
             ######################
-            # Passing the data through the networks
-            padded_images = padder(images)
-            feature_maps = netF(padded_images)
-            score_maps = netS(feature_maps)
-            outputs = F.upsample(score_maps, labels.size()[1:], mode='bilinear')
+            # Passing the data through the network
+            outputs = model(images)
 
             ######################
             # Computing the loss and doing back-propagation
-            loss = cross_entropy2d(outputs, labels)
+            loss = criterion(outputs, labels)
             loss.backward()
 
             ######################
@@ -140,7 +143,7 @@ def train(args):
             optimizerSS.step()
 
             if (i+1) % 20 == 0:
-                print("Iter [%d/%d], Epoch [%d/%d] Loss: %.4f" % (i+1, len(trainloader), epoch+1, args.n_epoch, loss.data[0]))
+                print("Iter [%d/%d], Epoch [%d/%d] Loss: %.4f" % (i+1, len(dataloader), epoch+1, args.n_epoch, loss.data[0]))
 
         # test_output = model(test_image)
         # predicted = loader.decode_segmap(test_output[0].cpu().data.numpy().argmax(0))
@@ -149,40 +152,35 @@ def train(args):
         # vis.image(test_image[0].cpu().data.numpy(), opts=dict(title='Input' + str(epoch)))
         # vis.image(np.transpose(target, [2,0,1]), opts=dict(title='GT' + str(epoch)))
         # vis.image(np.transpose(predicted, [2,0,1]), opts=dict(title='Predicted' + str(epoch)))
-        if args.netF_path != '':
-            torch.save(netF, "./{}/netF_{}_{}_{}_from_{}.pkl".format(args.save_folder, args.arch, args.dataset, epoch, args.netF_path))
-        else:
-            torch.save(netF, "./{}/netF_{}_{}_{}.pkl".format(args.save_folder, args.arch, args.dataset, epoch))
-        if args.netS_path != '':
-            torch.save(netS, "./{}/netS_{}_{}_{}_from_{}.pkl".format(args.save_folder, args.arch, args.dataset, epoch, args.netS_path))
-        else:
-            torch.save(netS, "./{}/netS_{}_{}_{}.pkl".format(args.save_folder, args.arch, args.dataset, epoch))
+        if (epoch+1) % 5 == 0:
+            if args.model_path != '':
+                torch.save(model, "./{}/model_{}_{}_{}_from_{}.pkl" .format(args.save_folder, args.arch, args.dataset, epoch, args.model_path))
+            else:
+                torch.save(model, "./{}/model_{}_{}_{}.pkl".format(args.save_folder, args.arch, args.dataset, epoch))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hyperparams')
     parser.add_argument('--arch', nargs='?', type=str, required=True,
                         help='Architecture to use [\'fcn8s, unet, segnet etc\']')
-    parser.add_argument('--dataset', nargs='?', type=str, default='pascal',
+    parser.add_argument('--dataset', nargs='?', type=str, default='videos',
                         help='Dataset to use [\'pascal, camvid, ade20k etc\']')
-    parser.add_argument('--img_rows', nargs='?', type=int, default=320,
+    parser.add_argument('--img_rows', nargs='?', type=int, default=224,
                         help='Height of the input image')
-    parser.add_argument('--img_cols', nargs='?', type=int, default=320,
+    parser.add_argument('--img_cols', nargs='?', type=int, default=224,
                         help='Height of the input image')
     parser.add_argument('--n_epoch', nargs='?', type=int, default=100,
                         help='# of the epochs')
-    parser.add_argument('--batch_size', nargs='?', type=int, default=1,
+    parser.add_argument('--batch_size', nargs='?', type=int, default=128,
                         help='Batch Size')
-    parser.add_argument('--l_rate', nargs='?', type=float, default=1e-5,
+    parser.add_argument('--l_rate', nargs='?', type=float, default=1e-2,
                         help='Learning Rate')
-    parser.add_argument('--netF_path', nargs='?', type=str, default='',
-                        help='path to the netF model')
-    parser.add_argument('--netS_path', nargs='?', type=str, default='',
-                        help='path to the netS model')
+    parser.add_argument('--model_path', nargs='?', type=str, default='',
+                        help='path to the model')
     parser.add_argument('--gpu', type=int, default=0,
                         help='which GPU to use')
     parser.add_argument('--split', nargs='?', type=str, default='train',
                         help='Split of dataset to test on')
-    parser.add_argument('--save_folder', nargs='?', type=str, default='saved',
+    parser.add_argument('--save_folder', nargs='?', type=str, default='saved_pretrained',
                         help='Where to save and retrieve the models')
     args = parser.parse_args()
     train(args)
