93da096bcbf4948c2319866f5ebdc8c1309e8a35
Mohammad Najafi <najafi.ece@gmail.com>
2017-10-06 20:16:49 +0100

    Added dcgan_alexnet that feasts on Imagenet dataset.
    The discriminator in this GAN (netD) is of type AlexNetGAN, defined in networks.py. AlexNetGAN is a fully convolutional AlexNet where Maxpooling layers are replaced by strides of two in the convolution layers. It also features Batchnorms and LeakyRelues.
    
    Once the GAN is trained for 1-2 epochs over Imagenet, we can check how good the trained alexnet works as pre-training for our semantic labeling purpose.
    Note that we need to modify the definitoin of our previously defined AlexFCNv2 to make it compatible with the structure of AlexNetGAN.

--- networks.py
+++ networks.py
@@ -34,3 +34,46 @@ class AlexNet(nn.Module):
         x = x.view(x.size(0), 256 * 6 * 6)
         x = self.classifier(x)
         return x
+
+
+class AlexNetGAN(nn.Module):
+
+    def __init__(self):
+        super(AlexNetGAN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.BatchNorm2d(num_features=64),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(64, 192, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm2d(192),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(192, 384, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(384),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Conv2d(256, 4096, kernel_size=7),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(4096, 4096, kernel_size=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(4096, 1, 1),
+            # The data shape is now (,1)
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x.view(-1, 1).squeeze(1)
